9fa780f1034242173d81a0a3fa6bbe333dc2f549
Jamesgit22 <jschroeder97@icloud.com>
2023-04-11 09:44:30 -0500

    Added document ready function to javascript file. Also added API fetch to Yelp Fusion

---
+++ assets/js/scripts.js
@@ -0,0 +1,21 @@
+// A $( document ).ready() block.
+$(document).ready(function () {
+  console.log("ready!");
+
+  // Fetch data from Yelp Fusion
+  async function getJSONData() {
+    const response = await fetch(
+      `https://floating-headland-95050.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=pensacola&radius=20000&categories=movietheater&sort_by=best_match&limit=20`,
+      {
+        method: "GET",
+        headers: {
+          Authorization:
+            "Bearer 6qR4_DRix9tgxR-wMhc5CZxksBaAi7-kad8A147whk73CqEO0g_2KhKnIyk8PkGGMuZAQIBF3VNetKKXTuqg1g0B-2dDLnu9FPV8-IA-j7W5fhsh5Jm_6-i-1S80ZHYx",
+        },
+      }
+    );
+    const jsonData = await response.json();
+    console.log(jsonData);
+  }
+  getJSONData();
+});
