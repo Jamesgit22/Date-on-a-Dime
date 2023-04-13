3d58def2a90c3134088f3ecaff9f6b0c177c64d0
Ashleyg5 <ashleygiraldo5@hotmail.com>
2023-04-13 14:25:15 -0400

    started working on local storage in both JS documents

--- assets/js/favdates.js
+++ assets/js/favdates.js
@@ -1,3 +1,10 @@
 // Check local storage for any saved date choices
 
-const storedDates = JSON.parse(localStorage.getItem("favorites"));
+let storage = JSON.parse(localStorage.getItem(`favorites`)) || []
+let dateContainer = document.querySelector(`.dateContainer`)
+for (let i = 0; i < storage.length; i++) {
+    let date = storage[i]
+    let inputt = document.createElement(`input`)
+    inputt.textContent = date
+    dateContainer.appendChild(inputt)
+}
